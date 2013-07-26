a196a303af3ec200cb0955e98450cf62a389a1c6
bvuongx <bvuong@gmail.com>
2013-07-26 14:18:20 -0700

    created a join table and show displays user email with question.

--- app/models/question.rb
+++ app/models/question.rb
@@ -1,3 +1,10 @@
 class Question < ActiveRecord::Base
+
   attr_accessible :answer_the_question, :ask_a_question
+  has_and_belongs_to_many :users
+
+  def has_been_updated_by_user?(user)
+    return self.users.include? user
+  end
+
 end
---
+++ db/migrate/20130726203708_create_questions_users_table.rb
@@ -0,0 +1,8 @@
+class CreateQuestionsUsersTable < ActiveRecord::Migration
+  def change
+    create_table :questions_users, id: false do |t|
+      t.integer :question_id
+      t.integer :user_id
+    end
+  end
+end
